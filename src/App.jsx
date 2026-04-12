a6b84ac82d9dd72667a7723562dd7af8b1f802c4
ankiibott <ankii93599@gmail.com>
2026-04-12 10:44:56 +0530

    components xplained

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello Guyss</h1>
+      <h3>this is my first jsx to work with</h3>
+    </div>
+  )
+}
+
+export default App
