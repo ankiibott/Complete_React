4431feafc254b51d97c8621d5830fca464c26d86
ankiibott <ankii93599@gmail.com>
2026-07-24 13:30:16 +0530

    created a folder for import export concept

---
+++ 1_import_export/app.js
@@ -0,0 +1,6 @@
+//named export
+export const arr = [1, 2, 3, 4, 5]
+export const age = 20
+const user = 'Ankit'
+//default export
+export default user
---
+++ 1_import_export/script.js
@@ -0,0 +1,5 @@
+//default import
+import username from './app.js'
+//named import
+import {arr,age} from './app.js'
+console.log(age,arr);
