8c198a2efc34ba785876ef2c10ae16e6e717aca7
ankiibott <ankii93599@gmail.com>
2026-08-01 11:04:50 +0530

    useeffect . state change can be seen not parallely

---
+++ 18-Gallery-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-black h-screen text-white'>
+      <button className='bg-green-600 px-5 py-2 rounded text-white '>get Data</button>
+    </div>
+  )
+}
+
+export default App
