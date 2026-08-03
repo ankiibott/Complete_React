920d4e706dd62dcee3e968b557eee4427c234895
ankiibott <ankii93599@gmail.com>
2026-08-03 08:01:27 +0530

    this project is incomplete

--- 18-Gallery-project/src/App.jsx
+++ 18-Gallery-project/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div className='bg-black h-screen text-white'>
-      <button className='bg-green-600 px-5 py-2 rounded text-white '>get Data</button>
+      <button className='bg-green-600 px-5 py-2 rounded  text-white '>get Data</button>
     </div>
   )
 }
